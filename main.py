3bd1011c600e3a2169335f2bb6c4e7fab7613789
Annddreyy <andreymail22112006@gmail.com>
2024-10-16 19:40:47 +0300

    Add get genres

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from flask import Flask, jsonify
 
-from db import get_connection
 from get_films import get_films_blueprint
+from get_genres import get_genres_blueprint
 
 app = Flask(__name__)
 
 app.register_blueprint(get_films_blueprint)
+app.register_blueprint(get_genres_blueprint)
 
 
 if __name__ == '__main__':
